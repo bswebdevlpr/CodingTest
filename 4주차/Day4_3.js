f5219efe46b7c47df85395afce9d29f2032eccb4
bswebdvlpr <tfl0324@gachon.ac.kr>
2022-07-18 13:35:12 +0900

    Day4_3.js 4th try

--- 4주차/Day4_3.js
+++ 4주차/Day4_3.js
@@ -13,7 +13,9 @@ function solution(info, edges) {
     const FOLLOWED = -1;
     class Node {
         constructor(type, parent, index){
-            this.checked = false;
+            this.inLamb = -1;  // 늑대 수 보존을 위한 변수
+            this.outLamb = -1;  // 부모노드, 자식노드 간 이동방향 판단을 위한 변수
+            
             this.index = index;
             this.type = type;
             this.childs = [];
@@ -31,152 +33,152 @@ function solution(info, edges) {
         nodeIdx[parent].childs.push(nodeIdx[child]);
         nodeIdx[child].parent = nodeIdx[parent];
     });
-    // console.log(nodeIdx);
+    console.log(nodeIdx);
     
-    let lambCnt = 0;
-    
-    function findMaxLambs(me, lambCnt, wolfCnt){
-        console.log(me.index, "=> ");
-        // FOLLOWED -> 통과해 / chekced -> 걸로 가지마
-        // 내꺼 따지고 -> 아래 따지고 -> 위로 돌아가기/끝내기
-        
-        if(me == root){
-            let checkCnt = 0;
-            for(let child of me.childs){
-                if(child.checked == true) checkCnt++;
-            }
-            
-            if(checkCnt == me.childs.length){
-                console.log("lambCnt="+lambCnt, "wolfCnt="+wolfCnt);
-                return lambCnt;
-            }
-        }
-        
-        // 현재 노드가 양
-        if(me.type == LAMB){
-            lambCnt += 1;
-            me.type = FOLLOWED;
-        } 
-        
-        // 현재 노드가 늑대
-        else if(me.type == WOLF){
-            wolfCnt += 1;
-            
-            /* 
-            부모노드로 이동
-            1) 늑대의 수가 양보다 많거나 같을 때
-            2) 다음 노드가 없을 때
-            */
-            if(wolfCnt >= lambCnt || me.childs.length == 0){
-                me.checked = true;
-                findMaxLambs(me.parent, lambCnt, wolfCnt-1);
-                return;
-            }
-            
-            // 다음 노드가 없는경우
-            me.type = FOLLOWED;
-        }
-        
-        // 현재 노드가 FOLLOWED (다시 거치게 되면)
-        // === 통과된 노드가 수행할 기능 ===
-        
-        /* 
-        1. 부모노드로 이동
-            1) 내 자식노드가 없으면
-            2) 자식노드가 전부 checked이면 (나를 checked로 바꿔줌)
-        */
-        if((me.childs.length == 0)){
-            me.checked = true;
-            findMaxLambs(me.parent, lambCnt, wolfCnt);
-            return;
-        } else {
-            if(me.childs.length == 1){
-                if(me.childs[0].checked){
-                    me.checked = true;
-                    findMaxLambs(me.parent, lambCnt, wolfCnt);
-                    return;
-                }
-            } else{
-                if(me.childs[0].checked && me.childs[1].checked){
-                    me.checked = true;
-                    findMaxLambs(me.parent, lambCnt, wolfCnt);
-                    return;
-                }
-            }
-        }
-        
-        // 2. 자식노드로 이동
-        // 첫번째 자식노드 -> 두번째 자식노드
-        if(!me.childs[0].checked){
-            findMaxLambs(me.childs[0], lambCnt, wolfCnt);
-        } else if(me.childs[1] && !me.childs[1].checked){
-            findMaxLambs(me.childs[1], lambCnt, wolfCnt);
-        }
-            
-        return;
-        // 늑대 노드가 checked이면 걸로 가지않음
-        // 내 자식노드가 없거나 전부 checked 이면 내 자신도 checked하기
-        
-    }
-//     function findMaxLambs(me, lambCnt, wolfCnt){
-//         console.log(me.index, "=> ");
-//         if(me.type != FOLLOWED){
-//             if(me.type == LAMB) lambCnt ++;
-//             else if(me.type == WOLF) wolfCnt ++;
-//             else return "ERROR";
+    let present = root;
+    let lambCnt = 1;
+    let wolfCnt = 0;
 
-//             me.type = FOLLOWED;
-//         }
-        
-//         // leaf-node인 경우 부모노드로 돌아감.
-//         if(me.childs.length === 0){
-//             findMaxLambs(me.parent, lambCnt, wolfCnt);
-//             return;
-//         }
-        
-//         // 늑대 카운트가 양 카운트보다 많으면 이전 노드로 돌아감.
-//         if(wolfCnt >= lambCnt){
-//             me.checked = true;
-//             me.type = WOLF;  // 부모 노드에서 다시 들리지 않기 위한 flag. 다른 노드로 진입하게 되면 다시 초기화 해줌.
-//             findMaxLambs(me.parent, lambCnt, wolfCnt - 1);
-//             return;
-//         }
-        
-//         // 내가 지나갈 수 있는 노드면 checked를 true로 바꿔주고 형제 노드의 checked를 false로 바꿔줌.
-//         me.checked = true;
-//         if(me.parent){
-//             const parent = me.parent;
-//             if(parent.childs[1] == me){
-//                 const sibling parent.childs[0];
-//                 sibling.checked = false;
-//             }
-//         }
-        
-//         // 들렀던 노드를 다시 들를 수 있다.
-//         // 한 자식노드로 들어가면 다른 자식노드의 체크여부를 free해줌.
-//         if(me.childs[0].type != FOLLOWED && !me.childs[0].checked){
-//             findMaxLambs(me.childs[0], lambCnt, wolfCnt);
-//             return;
-            
-//         } else if(me.childs[1].type != FOLLOWED && !me.childs[1].checked){
-//             findMaxLambs(me.childs[1], lambCnt, wolfCnt);
-//             return;
-            
-//         } else if (me.parent){
-//             me.type = info[me.index];
-//             me.checked = true;
-//             if(me.childs[0].type != FOLLOWED) me.childs[0].checked = false;
-//             if(me.childs[1].type != FOLLOWED) me.childs[1].checked = false;
-//             findMaxLambs(me.parent, lambCnt, wolfCnt);
-//             return;
-            
-//         } else {
-//             console.log("lambCnt="+lambCnt, "wolfCnt="+wolfCnt, "final location => "+me.index);
-//         }
-//     }
     
+    // 현재 노드에서 자식노드와 부모노드 즉, 세 방향을 고려하고 갈수있는 노드의 위치를 return.
+    function findMaxLambs(){
+        console.log("현재위치="+present.index, "type="+present.type, "inLamb="+present.inLamb, "lambCnt="+lambCnt, "wolfCnt="+wolfCnt);
+        let next;
+        
+        // 카운트는 부모에서 해버림.
+        
+        // 자식노드로 이동이 가능한지 보고(자식이 있는가? -> 자식으로 이동할 수 있는가?)
+        const childLen = present.childs.length;
+        if(childLen == 0){
+            // WOLF노드가 leaf-node일때, FOLLOWED일 때를 따지는 부분 이어서 해야함
+            
+            // Case - wolfCnt를 넘지 않아서 WOLF Node로 넘어왔는데 해당 노드가 leaf node인 경우.
+            if(info[present.index] == WOLF){
+                present.type = WOLF;
+                wolfCnt--;
+            }
+            present.outLamb = lambCnt;
+            next = present.parent;
+            
+        } else{
+            // 자식노드가 하나
+            if(childLen == 1){
+                // 초기값을 설정해놓았기 때문에 한번은 무조건 방문함.
+                
+                // 들어갈 lambCnt가 자식노드가 내보낸 값보다 크면 자식노드로 이동.
+                if(lambCnt > present.childs[0].outLamb){
+                    if(present.childs[0].type == LAMB) {
+                        lambCnt ++;
+                        next = present.childs[0];
+                        present.childs[0].type = FOLLOWED;
+                    }
+                    else if(present.childs[0].type == WOLF) {
+                        present.childs[0].inLamb = lambCnt;
+                        wolfCnt++;
+                        if(wolfCnt >= lambCnt){
+                            wolfCnt--;
+                            present.childs[0].outLamb = lambCnt;
+                            next = present.parent;
+                        } else {
+                            next = present.childs[0];
+                            present.childs[0].type = FOLLOWED;
+                        }
+                    } 
+                    // FOLLOWED인 경우. count 하지 않기 위함.
+                        
+                    
+                } else {  // 자식노드로 갈 수 없는 경우.
+                    
+                    // WOLF node에서 자식노드로 갈 수 없을 때, 본인을 카운트에서 제거하고 부모노드로 이동해야함.
+                    if(info[present.index] == WOLF && present.inLamb == lambCnt) {
+                        // 내 노드 아래로 양이 없으면 양이 증가한게 없다는 얘기이므로 내 노드의 늑대를 데려갈 필요가 없음. 따라서 내 wolfCnt도 하나 빼줌.
+                        present.type = WOLF;
+                        wolfCnt--;
+                    }
+                    
+                    present.outLamb = lambCnt;
+                    next = present.parent;
+                }
+                
+            } else if(childLen == 2){
+                // 지금 들어오는 값이 내가 내보낸 값보다 크면 자식노드로 이동.
+                if(lambCnt > present.childs[0].outLamb){
+                    if(present.childs[0].type == LAMB) {
+                        lambCnt++;
+                        next = present.childs[0];
+                        present.childs[0].type = FOLLOWED;
+                    }
+                    else if(present.childs[0].type == WOLF) {
+                        present.childs[0].inLamb = lambCnt;
+                        wolfCnt++;
+                        if(wolfCnt >= lambCnt){
+                            wolfCnt--;
+                            present.childs[0].outLamb = lambCnt;
+
+                            next = present;
+                        } else {
+                            next = present.childs[0];
+                            present.childs[0].type = FOLLOWED;
+                        }
+                    } else {
+                    // FOLLOWED인 경우. count 하지 않기 위함.
+                        next = present.childs[0];
+                    }
+                    
+                } else if(lambCnt > present.childs[1].outLamb){
+                    if(present.childs[1].type == LAMB) {
+                        lambCnt++;
+                        next = present.childs[1];
+                        present.childs[1].type = FOLLOWED;
+                    }
+                    else if(present.childs[1].type == WOLF) {
+                        present.childs[1].inLamb = lambCnt;
+                        wolfCnt++;
+                        if(wolfCnt >= lambCnt){
+                            wolfCnt--;
+                            present.childs[1].outLamb = lambCnt;
+                            next = present;
+                        } else {
+                            next = present.childs[1];
+                            present.childs[1].type = FOLLOWED;
+                        }
+                    } else {
+                    // FOLLOWED인 경우. count 하지 않기 위함.
+                        next = present.childs[1];
+                    }
+                    
+                } else {  // 자식노드로 갈 수 없는 경우
+                    
+                    // WOLF node에서 자식노드로 갈 수 없을 때, 본인을 카운트에서 제거하고 부모노드로 이동해야함.
+                    if(info[present.index] == WOLF && present.inLamb == lambCnt) {
+                    // 내 노드 아래로 양이 없으면 양이 증가한게 없다는 얘기이므로 내 노드의 늑대를 데려갈 필요가 없음. 따라서 내 wolfCnt도 하나 빼줌.
+                    present.type = WOLF;
+                    wolfCnt--;
+                    }
+                    
+                    present.outLamb = lambCnt;
+                    next = present.parent;
+                }
+                
+            }
+        }
+        
+        
+        return next;
+    }
     
-    var answer = findMaxLambs(nodeIdx[0], lambCnt, 0);
+    let i = 0;
+    while(i<100){
+        i++;
+        if(present == null){
+            console.log(i+"번째 시도:", "lambCnt="+lambCnt)
+            break;
+        }
+        present = findMaxLambs();
+    }
+    
+    var answer = lambCnt;
     return answer;
 }
-solution([0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1], [[0, 1], [1, 2], [1, 4], [0, 8], [8, 7], [9, 10], [9, 11], [4, 3], [6, 5], [4, 6], [8, 9]]);
+
+solution(	[0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0], [[0, 1], [0, 2], [1, 3], [1, 4], [2, 5], [2, 6], [3, 7], [4, 8], [6, 9], [9, 10]]);
