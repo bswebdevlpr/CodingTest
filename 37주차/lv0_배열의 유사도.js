1007dc924f03dba4a1cc11d1f9b53e6d07f02046
bswebdvlpr <tfl0324@gachon.ac.kr>
2023-04-15 23:21:02 +0900

    37주차

---
+++ 37주차/lv0_배열의 유사도.js
@@ -0,0 +1,14 @@
+function solution(s1, s2) {
+  var answer = 0;
+
+  s1.forEach((a) => {
+    s2.forEach((b) => {
+      if (a === b) {
+        answer++;
+        return;
+      }
+    });
+  });
+
+  return answer;
+}
