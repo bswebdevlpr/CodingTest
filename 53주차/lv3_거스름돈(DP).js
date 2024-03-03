45e60f30ca3f438cb3401994e34442c21efe2eba
bswebdvlpr <tfl0324@gachon.ac.kr>
2024-03-03 13:58:14 +0900

    lv3_거스름돈(DP) - not 2dim

--- 53주차/lv3_거스름돈(DP).js
+++ 53주차/lv3_거스름돈(DP).js
@@ -1,21 +1,17 @@
 function solution(n, money) {
-  const dp = new Array(money.length)
-    .fill()
-    .map(() => new Array(n + 1).fill(0).fill(1, 0, 1));
+  const dp = [1, ...new Array(n).fill(0)];
   // DP는 중복되는 연산을 이용하여 결과를 도출하는 방법임.
-  dp[0] = new Array(n + 1).fill(1);
 
   // i: 사용된 화폐종류
   // j: 거스름돈
-  for (let i = 1; i < money.length; i++) {
+  for (let i = 0; i < money.length; i++) {
     const coin = money[i];
-    for (let j = 1; j < n + 1; j++) {
-      dp[i][j] =
-        j < coin ? dp[i - 1][j] : (dp[i][j] = dp[i - 1][j] + dp[i][j - coin]);
+    for (let j = 0; j < n + 1; j++) {
+      if (j >= coin) dp[j] += dp[j - coin];
     }
   }
 
-  return dp[money.length - 1][n] % 1000000007;
+  return dp[n] % 1000000007;
 }
 
 solution(5, [1, 2, 5]);
