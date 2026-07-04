4f64f247a62878c9a95c742b6e95c090c5000da1
bswebdevlpr <tfl0324@gachon.ac.kr>
2026-07-04 20:39:43 +0900

    [DP] 62. Unique Paths (+ Bottom-Up)

--- LeetCode/dp/medium/62_Unique_Paths.js
+++ LeetCode/dp/medium/62_Unique_Paths.js
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/unique-paths/description/?envType=study-plan-v2&envId=leetcode-75
 
+// TOP-DOWN
 /**
  * @param {number} m
  * @param {number} n
@@ -35,4 +36,27 @@ var uniquePaths = function (m, n) {
   dfs(0, 0);
 
   return memo[0][0];
+};
+
+// BOTTOM-UP
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  // m x n 크기의 DP 테이블을 1로 초기화합니다.
+  // (첫 번째 행과 첫 번째 열은 무조건 직진밖에 없으므로 경로가 1개뿐입니다)
+  const dp = Array.from({ length: m }, () => Array(n).fill(1));
+
+  // 첫 행(row=0)과 첫 열(col=0)은 이미 1로 채워졌으므로 (1, 1)부터 시작합니다.
+  for (let row = 1; row < m; row++) {
+    for (let col = 1; col < n; col++) {
+      // 현재 칸으로 오는 방법 = 위에서 내려오는 방법 + 왼쪽에서 오는 방법
+      dp[row][col] = dp[row - 1][col] + dp[row][col - 1];
+    }
+  }
+
+  // 맨 오른쪽 아래 목적지의 값을 리턴합니다.
+  return dp[m - 1][n - 1];
 };
