c7870203726440a8acbcd95db64ff9e02ef63701
bswebdevlpr <tfl0324@gachon.ac.kr>
2026-07-12 20:23:24 +0900

    [Two Pointers] 11. Container With Most Water

---
+++ LeetCode/two-pointers/medium/11-Container-With-Most-Water.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  // 수조의 최대 부피 구하기
+  // 수렴형
+  // 가로가 줄던가 세로가 줄던가
+
+  let left = 0,
+    right = height.length - 1;
+  let volume = (right - left) * Math.min(height[left], height[right]);
+  while (left !== right) {
+    let width = right - left,
+      maxHeight = Math.min(height[left], height[right]);
+    volume = Math.max(volume, width * maxHeight);
+
+    if (height[left] < height[right]) left++;
+    else right--;
+  }
+
+  return volume;
+};
